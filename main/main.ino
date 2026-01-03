43d87fa1626544bf855c48ac9ef9d664cdc5738e
geopad <r.kulakov@edu.centraluniversity.ru>
2026-01-03 16:02:42 +0300

    Test led logic added. Did functions for logics.

--- main/main.ino
+++ main/main.ino
@@ -1,15 +1,36 @@
 #include <Arduino.h>
 #define KEY_COUNT 2
-#define FIRST_KEY_PIN 40
+#define FIRST_KEY_PIN 12
+#define FIRST_LED_PIN 24
+#define LAST_LED_PIN 42
+unsigned long timer1 = 1000 * 60;
 
-void setup() { Serial.begin(9600); }
+void setup() {
+  Serial.begin(9600);
+  for (int pin = 0; pin < 10; ++pin) {
+    pinMode(FIRST_LED_PIN + pin * 2, OUTPUT);
+  }
+}
 
-void loop() {
+void ButtonsLogic() {
   for (int i = 0; i < KEY_COUNT; ++i) {
-    int key_pin = FIRST_KEY_PIN + i * 2;
+    int key_pin = FIRST_KEY_PIN + i;
     bool key_up = digitalRead(key_pin);
     if (!key_up) {
       Serial.println("button pressed");
     }
   }
+}
+
+void LedLogic() {
+  unsigned long int ms = millis();
+  int pinn = FIRST_LED_PIN + 2 * ((ms / 100) % 10);
+  Serial.println("works");
+  digitalWrite(pinn, HIGH);
+  delay(5);
+  digitalWrite(pinn, LOW);
+}
+
+void loop() {
+  LedLogic();
 }
