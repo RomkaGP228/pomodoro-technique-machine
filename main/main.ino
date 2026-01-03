d15d1377df13f16f251a8a667f04fb55b2a74704
geopad <r.kulakov@edu.centraluniversity.ru>
2026-01-03 18:23:06 +0300

    Updated fully light logic, now its ready to set up technique. Did some special funcs

--- main/main.ino
+++ main/main.ino
@@ -1,20 +1,43 @@
 #include <Arduino.h>
+#include <LinkedList.h>
+
+// ПИНЫ
 #define KEY_COUNT 2
 #define FIRST_KEY_PIN 12
 #define FIRST_LED_PIN 24
 #define LAST_LED_PIN 42
-unsigned long timer1 = 1000 * 60;
 
+// КОНСТАНТЫ но не тип
+unsigned long timer1 = 1000 * 60;
+LinkedList<int> low_pins = LinkedList<int>();
+int pinn = FIRST_LED_PIN;
 void setup() {
   Serial.begin(9600);
-  for (int pin = 0; pin < 10; ++pin) {
-    pinMode(FIRST_LED_PIN + pin * 2, OUTPUT);
+
+  // пиним все диоды
+  for (int pin = FIRST_LED_PIN; pin <= LAST_LED_PIN; pin += 2) {
+    pinMode(pin, OUTPUT);
   }
+
+  // Сей блок нужен чтобы горели с самого начала все диоды
+  for (int pin = FIRST_LED_PIN; pin <= LAST_LED_PIN; pin += 2) {
+    digitalWrite(pin, HIGH);
+  }
+  delay(1024); // Чтобы в 0 момент горел первый диод
+}
+
+int findElement(LinkedList<int> &list, int value) {
+  for (int i = 0; i < list.size(); i++) {
+    if (list.get(i) == value) {
+      return 1; // Нашли! Возвращаем индекс
+    }
+  }
+  return 0; // Не нашли
 }
 
 void ButtonsLogic() {
   for (int i = 0; i < KEY_COUNT; ++i) {
-    int key_pin = FIRST_KEY_PIN + i;
+    int key_pin = FIRST_KEY_PIN + i * 2;
     bool key_up = digitalRead(key_pin);
     if (!key_up) {
       Serial.println("button pressed");
@@ -22,15 +45,25 @@ void ButtonsLogic() {
   }
 }
 
-void LedLogic() {
-  unsigned long int ms = millis();
-  int pinn = FIRST_LED_PIN + 2 * ((ms / 100) % 10);
-  Serial.println("works");
-  digitalWrite(pinn, HIGH);
-  delay(5);
-  digitalWrite(pinn, LOW);
+void UpdateLight() {
+  for (int pin = 0; pin < 10; ++pin) {
+      digitalWrite(FIRST_LED_PIN + 2 * pin, HIGH);
+    }
+    low_pins.clear();
+    pinn = FIRST_LED_PIN;
 }
 
-void loop() {
-  LedLogic();
+void LedLogic() {
+  low_pins.add(pinn);
+  digitalWrite(pinn, LOW);
+  pinn += 2;
+  if (low_pins.size() == 10) {
+    delay(1024); // нужно чтобы последний нормально потух
+    UpdateLight();
+  }
+  delay(1024); // нужен чтобы первый загорелся
 }
+
+void loop() { 
+  LedLogic();
+  }
